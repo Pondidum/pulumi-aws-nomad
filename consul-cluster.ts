b89fa8b8daca055f3c798b151c3011f6cea8df39
andy <andy.davies@reaktor.com>
2020-07-28 12:31:19 +0300

    consul: extract componentResource

---
+++ consul-cluster.ts
@@ -0,0 +1,184 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import { ComponentResource, ComponentResourceOptions } from "@pulumi/pulumi";
+
+export interface ConsulServerClusterArgs {
+  size: number;
+
+  subnets: string[];
+  additionalSecurityGroups?: string[];
+}
+
+export class ConsulServerCluster extends ComponentResource {
+  private readonly name: string;
+  private readonly subnets: string[];
+  private readonly clusterSize: number;
+  private readonly additionalSecurityGroups: string[];
+
+  constructor(
+    name: string,
+    args: ConsulServerClusterArgs,
+    opts?: ComponentResourceOptions
+  ) {
+    super("pondidum:aws-consul-cluster", name, {}, opts);
+
+    this.name = name;
+    this.subnets = args.subnets;
+    this.clusterSize = args.size;
+    this.additionalSecurityGroups = args.additionalSecurityGroups || [];
+
+    const profile = this.createInstanceProfile();
+    const sg = this.createSecurityGroup();
+
+    const ami = pulumi.output(
+      aws.getAmi(
+        {
+          mostRecent: true,
+          nameRegex: "consul-.*",
+          owners: ["self"],
+        },
+        { async: true }
+      )
+    );
+
+    const lc = new aws.ec2.LaunchConfiguration(
+      "consul",
+      {
+        namePrefix: this.name,
+        imageId: ami.imageId,
+        instanceType: aws.ec2.InstanceTypes.T2_Micro,
+        userData: `
+    #!/bin/bash
+    /opt/consul/bin/run-consul --server --cluster-tag-key "consul-servers" --cluster-tag-value "auto-join"`.trim(),
+
+        iamInstanceProfile: profile,
+        keyName: "karhu",
+        securityGroups: [sg, ...this.additionalSecurityGroups],
+
+        associatePublicIpAddress: true, //FOR NOW
+
+        rootBlockDevice: {
+          volumeType: "standard",
+          volumeSize: 50,
+          deleteOnTermination: true,
+        },
+      },
+      { parent: this }
+    );
+
+    const asg = new aws.autoscaling.Group(
+      "consul",
+      {
+        launchConfiguration: lc,
+
+        vpcZoneIdentifiers: this.subnets,
+
+        desiredCapacity: this.clusterSize,
+        minSize: this.clusterSize,
+        maxSize: this.clusterSize,
+
+        tags: [
+          { key: "Name", value: this.name, propagateAtLaunch: true },
+          {
+            key: "consul-servers",
+            value: "auto-join",
+            propagateAtLaunch: true,
+          },
+        ],
+      },
+      { parent: this }
+    );
+  }
+
+  private createSecurityGroup() {
+    const ports = [
+      { port: 8300, type: "tcp", name: "server rpc" },
+      { port: 8400, type: "tcp", name: "cli rpc" },
+      { port: 8301, type: "tcp", name: "serf lan" },
+      { port: 8301, type: "udp", name: "serf lan" },
+      { port: 8302, type: "tcp", name: "serf wan" },
+      { port: 8302, type: "udp", name: "serf wan" },
+      { port: 8500, type: "tcp", name: "http api" },
+      { port: 8600, type: "tcp", name: "dns" },
+      { port: 8600, type: "udp", name: "dns" },
+    ];
+
+    const group = new aws.ec2.SecurityGroup(
+      "consul",
+      {
+        namePrefix: this.name,
+        description: "consul server",
+
+        ingress: ports.map((p) => ({
+          fromPort: p.port,
+          toPort: p.port,
+          protocol: p.type,
+          self: true,
+          description: p.name,
+        })),
+
+        egress: [
+          { fromPort: 0, toPort: 0, protocol: "-1", cidrBlocks: ["0.0.0.0/0"] },
+        ],
+      },
+      { parent: this }
+    );
+
+    return group.id;
+  }
+
+  private async createInstanceProfile() {
+    const role = new aws.iam.Role(
+      "consul",
+      {
+        namePrefix: this.name,
+        assumeRolePolicy: {
+          Version: "2012-10-17",
+          Statement: [
+            {
+              Effect: "Allow",
+              Action: ["sts:AssumeRole"],
+              Principal: { Service: "ec2.amazonaws.com" },
+            },
+          ],
+        },
+      },
+      { parent: this }
+    );
+
+    const rolePolicy = new aws.iam.RolePolicy(
+      "consul",
+      {
+        namePrefix: this.name,
+        role: role,
+        policy: {
+          Version: "2012-10-17",
+          Statement: [
+            {
+              Effect: "Allow",
+              Resource: "*",
+              Action: [
+                "ec2:DescribeInstances",
+                "ec2:DescribeTags",
+                "autoscaling:DescribeAutoScalingGroups",
+              ],
+            },
+          ],
+        },
+      },
+      { parent: this }
+    );
+
+    const profile = new aws.iam.InstanceProfile(
+      "consul",
+      {
+        namePrefix: this.name,
+        path: "/",
+        role: role,
+      },
+      { parent: this }
+    );
+
+    return profile;
+  }
+}
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as vpcBuilder from "@jen20/pulumi-aws-vpc";
+
+import { ConsulServerCluster } from "./consul-cluster";
 
 async function main() {
   const availabilityZones = await aws.getAvailabilityZones({
@@ -31,117 +32,10 @@ async function main() {
 }
 
 async function consulCluster() {
-  const clusterSize = 3;
-  const clusterName = "consul-servers";
-
-  const ami = await aws.getAmi({
-    mostRecent: true,
-    nameRegex: "consul-.*",
-    owners: ["self"],
-  });
-
-  const role = new aws.iam.Role("consul", {
-    namePrefix: clusterName,
-    assumeRolePolicy: {
-      Version: "2012-10-17",
-      Statement: [
-        {
-          Effect: "Allow",
-          Action: ["sts:AssumeRole"],
-          Principal: { Service: "ec2.amazonaws.com" },
-        },
-      ],
-    },
-  });
-
-  const rolePolicy = new aws.iam.RolePolicy("consul", {
-    namePrefix: clusterName,
-    role: role,
-    policy: {
-      Version: "2012-10-17",
-      Statement: [
-        {
-          Effect: "Allow",
-          Resource: "*",
-          Action: [
-            "ec2:DescribeInstances",
-            "ec2:DescribeTags",
-            "autoscaling:DescribeAutoScalingGroups",
-          ],
-        },
-      ],
-    },
-  });
-
-  const profile = new aws.iam.InstanceProfile("consul", {
-    namePrefix: clusterName,
-    path: "/",
-    role: role,
-  });
-
-  const ports = [
-    { port: 8300, type: "tcp", name: "server rpc" },
-    { port: 8400, type: "tcp", name: "cli rpc" },
-    { port: 8301, type: "tcp", name: "serf lan" },
-    { port: 8301, type: "udp", name: "serf lan" },
-    { port: 8302, type: "tcp", name: "serf wan" },
-    { port: 8302, type: "udp", name: "serf wan" },
-    { port: 8500, type: "tcp", name: "http api" },
-    { port: 8600, type: "tcp", name: "dns" },
-    { port: 8600, type: "udp", name: "dns" },
-  ];
-
-  const lcgroup = new aws.ec2.SecurityGroup("consul", {
-    namePrefix: clusterName,
-    description: "consul server",
-
-    ingress: ports.map((p) => ({
-      fromPort: p.port,
-      toPort: p.port,
-      protocol: p.type,
-      self: true,
-      description: p.name,
-    })),
-
-    egress: [
-      { fromPort: 0, toPort: 0, protocol: "-1", cidrBlocks: ["0.0.0.0/0"] },
-    ],
-  });
-
-  const lc = new aws.ec2.LaunchConfiguration("consul", {
-    namePrefix: clusterName,
-    imageId: ami.imageId,
-    instanceType: aws.ec2.InstanceTypes.T2_Micro,
-    userData: `
-#!/bin/bash
-/opt/consul/bin/run-consul --server --cluster-tag-key "consul-servers" --cluster-tag-value "auto-join"`.trim(),
-
-    iamInstanceProfile: profile,
-    keyName: "karhu",
-    securityGroups: [lcgroup.id, "sg-0b9c74e28455f703a"],
-
-    associatePublicIpAddress: true, //FOR NOW
-
-    rootBlockDevice: {
-      volumeType: "standard",
-      volumeSize: 50,
-      deleteOnTermination: true,
-    },
-  });
-
-  const asg = new aws.autoscaling.Group("consul", {
-    launchConfiguration: lc,
-
-    vpcZoneIdentifiers: ["subnet-1d198d45"],
-
-    desiredCapacity: clusterSize,
-    minSize: clusterSize,
-    maxSize: clusterSize,
-
-    tags: [
-      { key: "Name", value: clusterName, propagateAtLaunch: true },
-      { key: "consul-servers", value: "auto-join", propagateAtLaunch: true },
-    ],
+  const consul = new ConsulServerCluster("consul", {
+    size: 3,
+    subnets: ["subnet-1d198d45"],
+    additionalSecurityGroups: ["sg-0b9c74e28455f703a"],
   });
 }
 
